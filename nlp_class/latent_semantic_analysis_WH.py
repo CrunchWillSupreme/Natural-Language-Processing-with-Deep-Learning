a5d51c7a2b7cee9dd0bbb8ad3e86174e8ce86ddd
Will Han <will.han38@gmail.com>
2020-10-06 20:51:42 -0400

    adding py files

---
+++ nlp_class/latent_semantic_analysis_WH.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  4 15:19:55 2020
+
+@author: willh
+"""
+%matplotlib auto # to pop images out of spyder
+%matplotlib inline # to show images in spyder console
+import nltk
+import numpy as np
+import matplotlib.pyplot as plt
+
+from nltk.stem import WordNetLemmatizer
+from sklearn.decomposition import TruncatedSVD
+
+wordnet_lemmatizer = WordNetLemmatizer()
+
+titles = [line.rstrip() for line in open('all_book_titles.txt')]
+
+stopwords = set(w.rstrip() for w in open('stopwords.txt'))
+stopwords = stopwords.union({'introduction', 'edition', 'series', 'application', 'approach', 'card', 'access', 'package', 'plus', 'etext', 'brief', 'vol', 'fundamental', 'guide', 'essential', 'printed', 'third', 'second', 'fourth'})
+
+def my_tokenizer(s):
+    s = s.lower() # downcase
+    tokens = nltk.tokenize.word_tokenize(s) # split string into words (tokens)
+    tokens = [t for t in tokens if len(t) > 2] # remove any words that are less than 3 characters, they're probably not useful
+    tokens = [wordnet_lemmatizer.lemmatize(t) for t in tokens] # put words into base form
+    tokens = [t for t in tokens if t not in stopwords] # remove stopwords
+    tokens = [t for t in tokens if not any(c.isdigit() for c in t)]
+    return tokens
+
+
+word_index_map = {}
+current_index = 0
+all_tokens = []
+all_titles = []
+index_word_map = []
+for title in titles:
+    try:
+        title = title.encode('ascii', 'ignore').decode('utf-8')
+        all_titles.append(title)
+        tokens = my_tokenizer(title)
+        all_tokens.append(tokens)
+        for token in tokens:
+            if token not in word_index_map:
+                word_index_map[token] = current_index
+                current_index += 1
+                index_word_map.append(token)
+    except:
+        pass
+                
+def token_to_vector(tokens):
+    x = np.zeros(len(word_index_map)) # add 1 for the label
+    for t in tokens:
+        i = word_index_map[t]
+        x[i] = 1
+
+    return x
+            
+N = len(all_tokens)
+D = len(word_index_map)
+X = np.zeros((D, N))
+i = 0
+for tokens in all_tokens:
+    X[:,i] = token_to_vector(tokens)
+    i += 1
+    
+svd = TruncatedSVD()
+Z = svd.fit_transform(X)
+plt.scatter(Z[:,0], Z[:,1])
+for i in range(D):
+    plt.annotate(s=index_word_map[i], xy = (Z[i,0], Z[i,1]))
+plt.show()
+    
